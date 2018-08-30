734dbe2e1dea429badf9ed2f0773c1e1d87a6531
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2018-08-30 12:00:35 -0300

    Update 1017.c

--- 1017.c
+++ 1017.c
@@ -14,10 +14,34 @@ int main ()
 
     quant_litros = double(velocidade * tempo) / 12;
 
-    printf("%.3f\n", quant_litros);
+    printf("%.3lf\n", quant_litros);
 
     return 0;
 
 }
 
+
+//Declarando a variável float
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main ()
+
+{
+    float velocidade; 
+    float tempo;
+    float quant_litros;
+
+    scanf("%f", &velocidade);
+    scanf("%f", &tempo);
+
+    quant_litros = (velocidade * tempo) / 12;
+
+    printf("%.3f\n", quant_litros);
+
+    return 0;
+
+}
 
