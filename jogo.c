ca62d6ed97eb36c1dca5e85bc4652e5711ab4d1e
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2018-09-07 19:08:11 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
-int main()
-{
-    int a,b,c,d,e,f;
-    scanf("%d %d %d %d",&a,&b,&c,&d);
-    if(c-a!=1 || d>b )
-    {
-    e = c - a;
-    if (e < 0)
-    {e = 23 + e;}
-    }
-    else
-        e=0;
-    f = d - b;
-    if (f < 0 )
-    {f = 60 + f;}
-    if (a==c && b==d) {
-	printf("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)\n");
-	}
-    else { 
-	printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",e,f);
-	}
+
+int main() {
     
+ int HI,HF,TEMPO;
+scanf("%d %d",&HI,&HF);
+TEMPO=24-HI+HF;
+ if (TEMPO >24)
+ printf("O JOGO DUROU %d HORA(S)\n",TEMPO-24);
+ else
+ printf("O JOGO DUROU %d HORA(S)\n",TEMPO);
+
     return 0;
-   
 }
-   
