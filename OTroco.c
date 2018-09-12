29b8883a3f5057e6c9d23d33c715c77636a0fcb5
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2018-09-11 21:40:12 -0300

    Create OTroco.c

---
+++ OTroco.c
@@ -0,0 +1,33 @@
+#include  <stdio.h>
+#include<math.h>
+int main ()
+{
+
+int t, n;
+double valor, d;
+
+    scanf("%d", &t);
+    for (; t > 0 ; t--)
+    {
+        scanf("%lf %d", &d, &n);
+
+        double troco = 0;
+
+        for (; n > 0 ; n--)
+        {
+            scanf("%lf",&valor);
+
+            double soma = valor;
+
+            while (soma + valor < d)
+                soma += valor;
+
+            if ( (d - soma) > troco)
+                troco = d - soma;
+        }
+
+        printf("%.2lf\n", troco);
+    }
+ 
+    return 0;
+}
