b10021c8d9f3ed837dc6fb57a65e746d0906ea44
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2018-09-13 16:37:01 -0300

    Create SemARG_Otroco.c

---
+++ SemARG_Otroco.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main() 
+{
+	int casos, marcas, i,j, sobrou;
+	float troco=0, valor, dinheiro, sobrou_f;
+    
+	scanf("%d",&casos);
+    
+	for(i=0;i<casos;i++){
+		scanf("%f%d",&dinheiro,&marcas);
+        
+		for(j=0;j<marcas;j++){
+			scanf("%f",&valor);
+            
+			sobrou = (int)dinheiro / valor;
+            
+			if (sobrou){
+				sobrou_f = dinheiro-(sobrou*valor);
+			}
+            
+			else{
+				sobrou_f = 0;
+			}
+            
+			if (troco<sobrou_f){
+				troco = sobrou_f;
+			}
+		}
+        
+		printf("%.2f\n",troco);
+		troco = 0;
+	}
+	return 0;
+}
