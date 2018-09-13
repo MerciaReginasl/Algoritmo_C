7f654f4dcd88012921b8f48753ef948201309362
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2018-09-13 16:34:14 -0300

    Update OTroco.c

--- OTroco.c
+++ OTroco.c
@@ -1,34 +1,37 @@
 //Por: https://github.com/Judenilson
-#include  <stdio.h>
-#include<math.h>
-int main ()
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[]) //argumentos
 {
-
-int t, n;
-double valor, d;
-
-    scanf("%d", &t);
-    for (; t > 0 ; t--)
-    {
-        scanf("%lf %d", &d, &n);
-
-        double troco = 0;
-
-        for (; n > 0 ; n--)
-        {
-            scanf("%lf",&valor);
-
-            double soma = valor;
-
-            while (soma + valor < d)
-                soma += valor;
-
-            if ( (d - soma) > troco)
-                troco = d - soma;
-        }
-
-        printf("%.2lf\n", troco);
-    }
- 
-    return 0;
+	int casos, marcas, i,j, sobrou;
+	float troco=0, valor, dinheiro, sobrou_f;
+    
+	scanf("%d",&casos);
+    
+	for(i=0;i<casos;i++){
+		scanf("%f%d",&dinheiro,&marcas);
+        
+		for(j=0;j<marcas;j++){
+			scanf("%f",&valor);
+            
+			sobrou = (int)dinheiro / valor;
+            
+			if (sobrou){
+				sobrou_f = dinheiro-(sobrou*valor);
+			}
+            
+			else{
+				sobrou_f = 0;
+			}
+            
+			if (troco<sobrou_f){
+				troco = sobrou_f;
+			}
+		}
+        
+		printf("%.2f\n",troco);
+		troco = 0;
+	}
+	return 0;
 }
